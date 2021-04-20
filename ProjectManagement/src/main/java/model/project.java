de9c79ed0e85ca3b2878bf454849fba670cb0f76
S.D.S.A.Fernando <shiranafernando1998@gmail.com>
2021-04-20 20:56:41 +0530

    DB Connection

--- ProjectManagement/src/main/java/model/project.java
+++ ProjectManagement/src/main/java/model/project.java
@@ -1,5 +1,21 @@
 package model;
 
-public class project {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class project {
+	private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.cj.jdbc.Driver");
+
+	 //Provide the correct details: DBServer/DBName, username, password
+	 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectmanagement", "root", "");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+	 }
 }
