171db04196f2ff37f4a6fbe163d6f13c64c696df
S.D.S.A.Fernando <shiranafernando1998@gmail.com>
2021-04-21 09:10:14 +0530

    Insert

--- ProjectManagement/src/main/java/com/ProjectService.java
+++ ProjectManagement/src/main/java/com/ProjectService.java
@@ -1,5 +1,32 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.project;
+@Path("/Projects")
 public class ProjectService {
+	project projectObj = new project();
+	
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertProject(@FormParam("authorName") String aname,
+	 @FormParam("projectCategory") String pcat,
+	 @FormParam("projectName") String pname,
+	 @FormParam("authorEmail") String aemail,
+	@FormParam("projectDesc") String pdesc)
+	{
+	 String output = projectObj.insertProject(aname, pcat, pname, aemail,pdesc);
+	return output;
+	}
+	
+
 
 }
--- ProjectManagement/src/main/java/model/project.java
+++ ProjectManagement/src/main/java/model/project.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class project {
 	private Connection connect()
@@ -13,10 +14,44 @@ public class project {
 
 	 //Provide the correct details: DBServer/DBName, username, password
 	 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectmanagement", "root", "");
-	 System.out.println("Connection successful");
 	 }
 	 catch (Exception e)
 	 {e.printStackTrace();}
 	 return con;
 	 }
+	
+	
+public String insertProject(String aname, String pcategory, String ptitile, String aemail, String pdesc)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for inserting."; }
+	 // create a prepared statement
+	 String query = "insert into project (`projectID`,`authorName`,`projectCategory`,`projectName`,`authorEmail`,`projectDesc`)"
+	 + " values (?, ?, ?, ?, ?,?)";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, 0);
+	 preparedStmt.setString(2, aname);
+	 preparedStmt.setString(3, pcategory);
+	 preparedStmt.setString(4, ptitile);
+	 preparedStmt.setString(5, aemail);
+	 preparedStmt.setString(6, pdesc);
+	// execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Inserted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while inserting the Project";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	
+	
 }
