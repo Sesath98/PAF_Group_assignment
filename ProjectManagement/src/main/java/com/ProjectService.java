bda02066e88c56be053317312e1eb0fec87d7509
S.D.S.A.Fernando <shiranafernando1998@gmail.com>
2021-04-21 11:11:49 +0530

    insert commit

--- ProjectManagement/src/main/java/com/ProjectService.java
+++ ProjectManagement/src/main/java/com/ProjectService.java
@@ -2,6 +2,7 @@ package com;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,17 +14,26 @@ public class ProjectService {
 	project projectObj = new project();
 	
 	
+	@GET
+	@Path("/view")
+	@Produces(MediaType.TEXT_HTML)
+	public String viewProjects()
+	 {
+		return projectObj.viewProjects(); 
+	 }
+	
 	@POST
-	@Path("/")
+	@Path("/insert")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String insertProject(@FormParam("authorName") String aname,
 	 @FormParam("projectCategory") String pcat,
 	 @FormParam("projectName") String pname,
+	 @FormParam("projectPrice")String pprice,
 	 @FormParam("authorEmail") String aemail,
 	@FormParam("projectDesc") String pdesc)
 	{
-	 String output = projectObj.insertProject(aname, pcat, pname, aemail,pdesc);
+	 String output = projectObj.insertProject(aname, pcat, pname,pprice, aemail,pdesc);
 	return output;
 	}
 	
--- ProjectManagement/src/main/java/model/project.java
+++ ProjectManagement/src/main/java/model/project.java
@@ -3,6 +3,8 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class project {
 	private Connection connect()
@@ -21,7 +23,7 @@ public class project {
 	 }
 	
 	
-public String insertProject(String aname, String pcategory, String ptitile, String aemail, String pdesc)
+public String insertProject(String aname, String pcategory, String ptitile,String price, String aemail, String pdesc)
 	 {
 	 String output = "";
 	 try
@@ -30,16 +32,17 @@ public String insertProject(String aname, String pcategory, String ptitile, Stri
 	 if (con == null)
 	 {return "Error while connecting to the database for inserting."; }
 	 // create a prepared statement
-	 String query = "insert into project (`projectID`,`authorName`,`projectCategory`,`projectName`,`authorEmail`,`projectDesc`)"
-	 + " values (?, ?, ?, ?, ?,?)";
+	 String query = "insert into project (`projectID`,`authorName`,`projectCategory`,`projectName`,`projectPrice`,`authorEmail`,`projectDesc`)"
+	 + " values (?, ?, ?, ?, ?,?,?)";
 	 PreparedStatement preparedStmt = con.prepareStatement(query);
 	 // binding values
 	 preparedStmt.setInt(1, 0);
 	 preparedStmt.setString(2, aname);
 	 preparedStmt.setString(3, pcategory);
 	 preparedStmt.setString(4, ptitile);
-	 preparedStmt.setString(5, aemail);
-	 preparedStmt.setString(6, pdesc);
+	 preparedStmt.setDouble(5, Double.parseDouble(price));
+	 preparedStmt.setString(6, aemail);
+	 preparedStmt.setString(7, pdesc);
 	// execute the statement
 	 preparedStmt.execute();
 	 con.close();
@@ -53,5 +56,62 @@ public String insertProject(String aname, String pcategory, String ptitile, Stri
 	 return output;
 	 }
 	
+public String viewProjects()
+{
+String output = "";
+try
+{
+Connection con = connect();
+if (con == null)
+{return "Did'nt connect to the database"; }
+// Prepare the html table to be displayed
+output = "<table border='1'><tr><th>Author Name</th><th>Project Category</th>" +
+"<th>Project Name</th>" +
+"<th>Project Price</th>" +		
+"<th>Author Email ($)</th>" +
+"<th>Project Description<th>" +
+"<th>Update</th>"+
+"<th>Remove</th></tr>";
+
+String query = "select * from project";
+Statement stmt = con.createStatement();
+ResultSet rs = stmt.executeQuery(query);
+// iterate through the rows in the result set
+while (rs.next())
+{
+String projectID  = Integer.toString(rs.getInt("projectID"));
+String authorName = rs.getString("authorName");
+String projectCategory = rs.getString("projectCategory");
+String projectName = rs.getString("projectName");
+String projectPrice = Double.toString(rs.getDouble("projectPrice"));
+String authorEmail = rs.getString("authorEmail");
+String projectDesc = rs.getString("projectDesc");
+// Add into the html table
+output += "<tr><td>" + projectID + "</td>";
+output += "<td>" + authorName + "</td>";
+output += "<td>" + projectCategory + "</td>";
+output += "<td>" + projectName + "</td>";
+output += "<td>" + projectPrice + "</td>";
+output += "<td>" + authorEmail + "</td>";
+output += "<td>" + projectDesc + "</td>";
+// buttons
+output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
++ "<td><form method='post' action='items.jsp'>"
++ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
++ "<input name='itemID' type='hidden' value='" + projectID 
++ "'>" + "</form></td></tr>";
+}
+con.close();
+// Complete the html table
+output += "</table>";
+}
+catch (Exception e)
+{
+output = "Error while reading the items.";
+System.err.println(e.getMessage());
+}
+return output;
+}
+
 	
 }
